8b3f4e37a467dd06d2ef2dbed703e9f4369a8d30
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-12-14 22:35:23 -0300

    Update model.py

--- gaby/models/model.py
+++ gaby/models/model.py
@@ -1,20 +1,32 @@
 from database import db
 
 class Funcionario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) 
+    #coluna id sendo definida como primaria
     nome = db.Column(db.String(80), nullable=False)
+    #coluna com o nome dos funcionários (limite de string com 80 char)
     cargo = db.Column(db.String(120), nullable=False)
+    #coluna com o cargo dos funcionários (limite de string com 120 char)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    #coluna com o email dos funcionários, definido como unico (limite de string com 120 char)
     senha = db.Column(db.String(120), unique=True, nullable=False)
+    #coluna com a senha dos funcionários sendo definida como unica (limite de string com 120 char)
     cpf = db.Column(db.String(20), unique=True,nullable=False)
-    salario = db.Column(db.Float, nullable=False)  
+    #coluna com o cpf dos funcionários sendo definida como unica (limite de string com 20 char)
+    salario = db.Column(db.Float, nullable=False) 
+    #coluna com o salario dos funcionários, definido como numero real
 
 class Prato(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+     id = db.Column(db.Integer, primary_key=True)
+    #coluna id sendo definida como primaria
     nome = db.Column(db.String(80), unique=True,nullable=False)
+    #coluna com o nome dos pratos (limite de string com 80 char)
     descr = db.Column(db.String(120), unique=True,nullable=False)
+    #coluna com a descrição dos pratos (limite de string com 80 char)
     preco = db.Column(db.Float, nullable=False)
+    #coluna com o preço dos pratos, definido como real
     imagem = db.Column(db.String(120))
+    #coluna com a imagem dos pratos (limite de string com 80 char)
 
 usuarios = {
     "user": "1234",
