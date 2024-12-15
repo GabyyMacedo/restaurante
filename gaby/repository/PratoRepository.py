711cce3f9107299d5ba8f40069ddb341f4b78619
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-12-15 18:26:46 -0300

    Update PratoRepository.py

--- gaby/repository/PratoRepository.py
+++ gaby/repository/PratoRepository.py
@@ -11,18 +11,18 @@ class PratoRepository:
     #chama o metodo listar_prato e retorna os dados
         return self.pratoDao.listar_pratos()
 
-    def get_prato(self, prato_id):
+    def get_prato(self, id):
     #chama o metodo get_prato, passa o id requerido e retorna
-        return self.pratoDao.get_prato(prato_id)
+        return self.pratoDao.get_prato(id)
 
-    def create_prato(self, prato_id, name, descr, preco):
+    def create_prato(self, id, nome, descr, preco):
     #chama o metodo add_prato e passa os dados requeridos e retorna
-        return self.pratoDao.add_prato(prato_id, name, descr, preco)
+        return self.pratoDao.add_prato(id, nome, descr, preco)
 
-    def update_prato(self, prato_id, name, descr, preco):
+    def update_prato(self, id, nome, descr, preco):
     #chama o metodo att_prato e passa os dados requeridos e retorna
-        return self.pratoDao.att_prato(prato_id, name, descr, preco)
+        return self.pratoDao.att_prato(id, nome, descr, preco)
 
-    def delete_prato(self, prato_id):
+    def delete_prato(self, id):
     #chama o metodo del_prato e passa os dados requeridos e retorna
-        return self.pratoDao.del_prato(prato_id)
+        return self.pratoDao.del_prato(id)
